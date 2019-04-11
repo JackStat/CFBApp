5c66370cc3ab6bc5a2e32a2c3345b4b22fac4d60
Tyler Hunt <thunt@snapfinance.com>
2019-04-10 19:42:22 -0600

    finishing the app for now

--- ui.R
+++ ui.R
@@ -5,10 +5,20 @@ library(shinydashboard)
 dashboardPage(
   dashboardHeader(),
   dashboardSidebar(
-    selectInput("team", "Team:", choices = sort(latestGames$Team))
+    selectInput(
+      "team"
+      , "Team:"
+      , choices = sort(latestGames$Team)
+      , selected = "Utah")
   ),
   dashboardBody(
     fluidRow(
+       valueBoxOutput("AvePoints", width = 3)
+      ,valueBoxOutput("AvePA", width = 3)
+      ,valueBoxOutput("Wins", width = 3)
+      ,valueBoxOutput("Losses", width = 3)
+    )
+    ,fluidRow(
       box(DT::dataTableOutput("table"), width = 12)
       # ,box(plotOutput("graph"))
     )
